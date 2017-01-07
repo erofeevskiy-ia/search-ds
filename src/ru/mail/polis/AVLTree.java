dfcdc0967a42fbc2bf650741244e8a85415986ea
erofeevskiy-ia <igorer27@yandex.ru>
2017-01-08 02:59:20 +0300

    avl+rb without optimizing in methods add and remove

--- src/ru/mail/polis/AVLTree.java
+++ src/ru/mail/polis/AVLTree.java
@@ -1,12 +1,38 @@
 package ru.mail.polis;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 //TODO: write code here
 public class AVLTree<E extends Comparable<E>> implements ISortedSet<E> {
 
-//    private Node root;
+    private Node root;
+
+    private class Node {
+        private final E key;      // the key
+        private int height;      // height of the subtree
+        //private int size;        // number of nodes in subtree
+        private Node left;       // left subtree
+        private Node right;      // right subtree
+
+        public Node(E key) {
+            this.key = key;
+            //this.size = size;
+           // this.height = height;
+        }
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder("N{");
+            sb.append("d=").append(key);
+            if (left != null) {
+                sb.append(", l=").append(left);
+            }
+            if (right != null) {
+                sb.append(", r=").append(right);
+            }
+            sb.append('}');
+            return sb.toString();
+        }
+    }
     private int size;
     private final Comparator<E> comparator;
 
@@ -20,45 +46,303 @@ public class AVLTree<E extends Comparable<E>> implements ISortedSet<E> {
 
     @Override
     public E first() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("set is empty, no first element");
+        }
+        Node curr = root;
+        while (curr.left != null) {
+            curr = curr.left;
+        }
+        return curr.key;
     }
 
     @Override
     public E last() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("set is empty, no last element");
+        }
+        Node curr = root;
+        while (curr.right != null) {
+            curr = curr.right;
+        }
+        return curr.key;
     }
 
     @Override
     public List<E> inorderTraverse() {
-        return null;
+        List<E> list = new ArrayList<E>(size);
+        inorderTraverse(root, list);
+        return list;
+    }
+
+    private void inorderTraverse(Node curr, List<E> list) {
+        if (curr == null) {
+            return;
+        }
+        inorderTraverse(curr.left, list);
+        list.add(curr.key);
+        inorderTraverse(curr.right, list);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
+//    private int size(Node x) {
+//        if (x == null) return 0;
+//        return x.size;
+//    }
     @Override
     public boolean isEmpty() {
-        return false;
+        return root == null;
     }
 
     @Override
-    public boolean contains(E value) {
+    public boolean contains(E key) {
+        if (key == null) {
+            throw new NullPointerException("value is null");
+        }
+        if (root != null) {
+            Node curr = root;
+            while (curr != null) {
+                int cmp = compare(curr.key, key);
+                if (cmp == 0) {
+                    return true;
+                } else if (cmp < 0) {
+                    curr = curr.right;
+                } else {
+                    curr = curr.left;
+                }
+            }
+        }
         return false;
     }
+
+    public int height() {
+        return height(root);
+    }
+
+    private int height(Node curr) {
+        if (curr == null) return -1;
+        return curr.height;
+    }
+
+    int balanceFactor(Node curr)
+    {
+        return height(curr.right)-height(curr.left);
+    }
+
+    void fixHeight(Node curr)
+    {
+        int hl = height(curr.left);
+        int hr = height(curr.right);
+        curr.height= (hl>hr?hl:hr)+1;
+    }
+
+    private Node rotateRight(Node curr) {
+        Node q = curr.left;
+        curr.left = q.right;
+        q.right = curr;
+        //q.size = curr.size;
+        //curr.size = 1 + size(curr.left) + size(curr.right);
+        fixHeight(curr);
+        fixHeight(q);
+        return q;
+    }
+
+    private Node rotateLeft(Node curr) // левый поворот вокруг curr
+    {
+        Node p = curr.right;
+        curr.right = p.left;
+        p.left = curr;
+        //p.size = curr.size;
+        //curr.size = 1 + size(curr.left) + size(curr.right);
+        fixHeight(curr);
+        fixHeight(p);
+        return p;
+    }
+
+    private Node balance(Node curr) // балансировка узла p
+    {
+        fixHeight(curr);
+        if( balanceFactor(curr)==2 )
+        {
+            if( balanceFactor(curr.right) < 0 )
+                curr.right = rotateRight(curr.right);
+            return rotateLeft(curr);
+        }
+        if( balanceFactor(curr)==-2 )
+        {
+            if( balanceFactor(curr.left) > 0  )
+                curr.left = rotateLeft(curr.left);
+            return rotateRight(curr);
+        }
+        return curr; // балансировка не нужна
+    }
+
 
     @Override
-    public boolean add(E value) {
-        return false;
+    public boolean add(E value) { // могут быть проблемы с балансировкой и т.д
+        if (value == null) {
+            throw new NullPointerException("value is null");
+        }
+        if (root == null) {
+            root = new Node(value);
+        } else {
+            Node curr = root;
+            while (true) {
+                int cmp = compare(curr.key, value);
+                if (cmp == 0) {
+                    return false;
+                } else if (cmp < 0) {
+                    if (curr.right != null) {
+                        curr = curr.right;
+                        balanceFactor(curr);
+                    } else {
+                        curr.right = new Node(value);
+                        break;
+                    }
+                } else if (cmp > 0) {
+                    if (curr.left != null) {
+                        curr = curr.left;
+                        balanceFactor(curr);
+                    } else {
+                        curr.left = new Node(value);
+                        break;
+                    }
+                }
+            }
+        }
+        size++;
+        return true;
     }
-
+    private boolean cont;
     @Override
     public boolean remove(E value) {
-        return false;
+        if (value == null) throw new NullPointerException("argument to delete() is null");
+
+        cont=false;
+        root = delete(root, value);
+        if(cont!=false)  {
+            size--;
+        }
+        return cont;
+
+        /*if (!contains(value)) return false;
+        root = delete(root, value);
+        size--;
+        return true;*/
     }
+
+    private Node delete(Node x, E key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = delete(x.left, key);
+        }
+        else if (cmp > 0) {
+            x.right = delete(x.right, key);
+        }
+        else {
+            cont=true;
+            if (x.left == null) {
+                return x.right;
+            }
+            else if (x.right == null) {
+                return x.left;
+            }
+            else {
+                Node y = x;
+                x = min(y.right);
+                x.right = deleteMin(y.right);
+                x.left = y.left;
+            }
+        }
+        //x.size = 1 + size(x.left) + size(x.right);
+        fixHeight(x);
+        return balance(x);
+    }
+
+    public void deleteMin() {
+        if (isEmpty()) throw new NoSuchElementException("called deleteMin() with empty symbol table");
+        root = deleteMin(root);
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) return x;
+        return min(x.left);
+    }
+
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        //x.size = 1 + size(x.left) + size(x.right);
+        fixHeight(x);
+
+        return balance(x);
+    }
+
+
 
     private int compare(E v1, E v2) {
         return comparator == null ? v1.compareTo(v2) : comparator.compare(v1, v2);
     }
+
+    @Override
+    public String toString() {
+        return "AVL{" + root + "}";
+    }
+
+    /*public static void main(String[] args) {
+        *//*AVLTree<Integer> tree = new AVLTree<>();
+        tree.add(10);
+        tree.add(5);
+        tree.add(15);
+        System.out.println(tree.inorderTraverse());
+        System.out.println(tree.size);
+        System.out.println(tree);
+        tree.remove(10);
+        tree.remove(15);
+        System.out.println(tree.inorderTraverse());
+        System.out.println(tree.size);
+        System.out.println(tree);
+        tree.remove(5);
+        System.out.println(tree.inorderTraverse());
+        System.out.println(tree.size);
+        System.out.println(tree);
+        tree.add(15);
+        System.out.println(tree.inorderTraverse());
+        System.out.println(tree.size);
+        System.out.println(tree);
+
+        System.out.println("------------");
+        Random rnd = new Random();
+        tree = new AVLTree<>();
+        for (int i = 0; i < 15; i++) {
+            tree.add(rnd.nextInt(50));
+        }
+        System.out.println(tree.inorderTraverse());
+        tree = new AVLTree<>((v1, v2) -> {
+            // Even first
+            final int c = Integer.compare(v1 % 2, v2 % 2);
+            return c != 0 ? c : Integer.compare(v1, v2);
+        });
+        for (int i = 0; i < 15; i++) {
+            tree.add(rnd.nextInt(50));
+        }
+        System.out.println(tree.inorderTraverse());*//*
+        AVLTree<Integer> set = new AVLTree<Integer>();
+        TreeSet<Integer> OK = new TreeSet<>();
+        for (int i = 0; i < 10; i++) {
+            assert set.add(i) == OK.add(i);
+        }
+        for (int i = 10; i >= 0; i--) {
+            assert set.remove(i) == OK.remove(i);
+            assert set.contains(i) == OK.contains(i);
+        }
+    }*/
 }
--- src/ru/mail/polis/RedBlackTree.java
+++ src/ru/mail/polis/RedBlackTree.java
@@ -1,10 +1,31 @@
 package ru.mail.polis;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 //TODO: write code here
 public class RedBlackTree<E extends Comparable<E>> implements ISortedSet<E> {
+
+    private static final boolean RED   = true;
+    private static final boolean BLACK = false;
+
+    private Node root;
+
+    private class Node {
+        private E key;           // key
+
+        private Node left, right;  // links to left and right subtrees
+        private boolean color;     // color of parent link
+       // private int size;          // subtree count
+
+        public Node(E key, boolean color, int size) {
+            this.key = key;
+            this.color = color;
+            //this.size = size;
+        }
+    }
 
     private int size;
     private final Comparator<E> comparator;
@@ -17,47 +38,287 @@ public class RedBlackTree<E extends Comparable<E>> implements ISortedSet<E> {
         this.comparator = comparator;
     }
 
+    private boolean isRed(Node x) {
+        if (x == null) return false;
+        return x.color == RED;
+    }
+
     @Override
     public E first() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("set is empty, no first element");
+        }
+        Node curr = root;
+        while (curr.left != null) {
+            curr = curr.left;
+        }
+        return curr.key;
     }
 
     @Override
     public E last() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("set is empty, no last element");
+        }
+        Node curr = root;
+        while (curr.right != null) {
+            curr = curr.right;
+        }
+        return curr.key;
     }
 
     @Override
     public List<E> inorderTraverse() {
-        return null;
+        List<E> list = new ArrayList<E>(size);
+        inorderTraverse(root, list);
+        return list;
+    }
+
+    private void inorderTraverse(Node curr, List<E> list) {
+        if (curr == null) {
+            return;
+        }
+        inorderTraverse(curr.left, list);
+        list.add(curr.key);
+        inorderTraverse(curr.right, list);
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return root==null;
     }
 
     @Override
-    public boolean contains(E value) {
+    public boolean contains(E key) {
+        if (key == null) {
+            throw new NullPointerException("value is null");
+        }
+        if (root != null) {
+            Node curr = root;
+            while (curr != null) {
+                int cmp = compare(curr.key, key);
+                if (cmp == 0) {
+                    return true;
+                } else if (cmp < 0) {
+                    curr = curr.right;
+                } else {
+                    curr = curr.left;
+                }
+            }
+        }
         return false;
     }
 
-    @Override
-    public boolean add(E value) {
-        return false;
+    private Node rotateRight(Node h) {
+
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = x.right.color;
+        x.right.color = RED;
+       // x.size = h.size;
+        //h.size = size(h.left) + size(h.right) + 1;
+        return x;
     }
 
+    private Node rotateLeft(Node h) {
+        // assert (h != null) && isRed(h.right);
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = x.left.color;
+        x.left.color = RED;
+        //x.size = h.size;
+       // h.size = size(h.left) + size(h.right) + 1;
+        return x;
+    }
+
+    private void flipColors(Node h) {
+        // h must have opposite color of its two children
+        // assert (h != null) && (h.left != null) && (h.right != null);
+        // assert (!isRed(h) &&  isRed(h.left) &&  isRed(h.right))
+        //    || (isRed(h)  && !isRed(h.left) && !isRed(h.right));
+        h.color = !h.color;
+        h.left.color = !h.left.color;
+        h.right.color = !h.right.color;
+    }
+    private boolean contAdd;
     @Override
-    public boolean remove(E value) {
-        return false;
+    public boolean add(E key) {
+        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+
+        if(contains(key)) return false;
+        root = put(root, key);
+        root.color = BLACK;
+        size++;
+        return true;
+    }
+
+    private Node put(Node h, E key) {
+        if (h == null) return new Node(key, RED, 1);
+
+        int cmp = key.compareTo(h.key);
+        if      (cmp < 0) h.left  = put(h.left,  key);
+        else if (cmp > 0) h.right = put(h.right, key);
+        else contAdd=false;
+
+
+        // fix-up any right-leaning links
+        if (isRed(h.right) && !isRed(h.left))      h = rotateLeft(h);
+        if (isRed(h.left)  &&  isRed(h.left.left)) h = rotateRight(h);
+        if (isRed(h.left)  &&  isRed(h.right))     flipColors(h);
+       // h.size = size(h.left) + size(h.right) + 1;
+
+        return h;
+    }
+
+    private Node moveRedLeft(Node h) {
+        // assert (h != null);
+        // assert isRed(h) && !isRed(h.left) && !isRed(h.left.left);
+
+        flipColors(h);
+        if (isRed(h.right.left)) {
+            h.right = rotateRight(h.right);
+            h = rotateLeft(h);
+            flipColors(h);
+        }
+        return h;
+    }
+
+    // Assuming that h is red and both h.right and h.right.left
+    // are black, make h.right or one of its children red.
+    private Node moveRedRight(Node h) {
+        // assert (h != null);
+        // assert isRed(h) && !isRed(h.right) && !isRed(h.right.left);
+        flipColors(h);
+        if (isRed(h.left.left)) {
+            h = rotateRight(h);
+            flipColors(h);
+        }
+        return h;
+    }
+
+
+
+    @Override
+    public boolean remove(E key) {
+
+        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
+        if (!contains(key)) return false;
+
+        // if both children of root are black, set root to red
+        if (!isRed(root.left) && !isRed(root.right))
+            root.color = RED;
+
+        root = delete(root, key);
+        if (!isEmpty()) root.color = BLACK;
+        size--;
+        return true;
+        // assert check();
+    }
+
+    // delete the key-value pair with the given key rooted at h
+    private Node delete(Node h, E key) {
+        // assert get(h, key) != null;
+
+        if (key.compareTo(h.key) < 0)  {
+            if (!isRed(h.left) && !isRed(h.left.left))
+                h = moveRedLeft(h);
+            h.left = delete(h.left, key);
+        }
+        else {
+            if (isRed(h.left))
+                h = rotateRight(h);
+            if (key.compareTo(h.key) == 0 && (h.right == null))
+                return null;
+            if (!isRed(h.right) && !isRed(h.right.left))
+                h = moveRedRight(h);
+            if (key.compareTo(h.key) == 0) {
+                Node x = min(h.right);
+                h.key = x.key;
+
+                // h.val = get(h.right, min(h.right).key);
+                // h.key = min(h.right).key;
+                h.right = deleteMin(h.right);
+            }
+            else h.right = delete(h.right, key);
+        }
+        return balance(h);
+    }
+
+    public void deleteMin() {
+        if (isEmpty()) throw new NoSuchElementException("BST underflow");
+
+        // if both children of root are black, set root to red
+        if (!isRed(root.left) && !isRed(root.right))
+            root.color = RED;
+
+        root = deleteMin(root);
+        if (!isEmpty()) root.color = BLACK;
+        // assert check();
+    }
+
+    // delete the key-value pair with the minimum key rooted at h
+    private Node deleteMin(Node h) {
+        if (h.left == null)
+            return null;
+
+        if (!isRed(h.left) && !isRed(h.left.left))
+            h = moveRedLeft(h);
+
+        h.left = deleteMin(h.left);
+        return balance(h);
+    }
+
+    private Node balance(Node h) {
+        // assert (h != null);
+
+        if (isRed(h.right))                      h = rotateLeft(h);
+        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
+        if (isRed(h.left) && isRed(h.right))     flipColors(h);
+
+        return h;
+    }
+
+    public E min() {
+        if (isEmpty()) throw new NoSuchElementException("called min() with empty symbol table");
+        return min(root).key;
+    }
+
+    // the smallest key in subtree rooted at x; null if no such key
+    private Node min(Node x) {
+        // assert x != null;
+        if (x.left == null) return x;
+        else                return min(x.left);
     }
 
     private int compare(E v1, E v2) {
         return comparator == null ? v1.compareTo(v2) : comparator.compare(v1, v2);
+    }
+
+    public static void main(String[] args) {
+        RedBlackTree<Integer> tree = new RedBlackTree<>();
+        tree.add(10);
+        tree.add(5);
+        tree.add(15);
+        System.out.println(tree.inorderTraverse());
+        System.out.println(tree.size);
+        System.out.println(tree);
+        tree.remove(10);
+        tree.remove(15);
+        System.out.println(tree.size);
+        System.out.println(tree);
+        tree.remove(5);
+        System.out.println(tree.size);
+        System.out.println(tree);
+        tree.add(15);
+        System.out.println(tree.size);
+        System.out.println(tree);
+
     }
 }
